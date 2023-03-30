6604c2c7943e2f2bb89e90fec17ce0d4a84a65ea
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-03-30 15:54:05 +0200

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -25,8 +25,44 @@ st.set_page_config(
     layout="wide"
 )
 
-
 st.title("Info")
+
+st.header("Istructions")
+
+st.write("Before using the deep learning model is necessary to make a calculation of clinopyroxene components and check on analysis quality as described in [].")
+df_calc = pd.read_excel('pages/Calculation.xlsx') 
+df_calc_xlsx = to_excel(df_calc)
+st.download_button(label='Download an empty form here!',
+                                data=df_calc_xlsx ,
+                                file_name= 'Calculation.xlsx')
+
+st.write("To carry out the processing it is necessary to download the ".xlsx" file with the botton above and follow the subsequent steps, each relating to a sheet of the file.")
+st.markdown("- data input: Clinopyroxene analyses. Input the analyses (paste special values) as indicated in the example (blank cell if the oxide has not been analysed or is below detection limit)")
+st.markdown("- calculation-1:	Calculation of clinopyroxene formula based on 4 cations and Fe3+ on charge balance. Do use the fill down command from column A to column CR to perform the calculation on all the analyses")
+st.markdown("- calculation-2:	Calculation of clinopyroxene components. Do use the fill down command from column A to FF to perform the calculation on all the analyses")
+st.markdown("- data output: -	Do use the fill down command from column A to BV to perform the check on all the analyses")
+st.markdown("- data sorting -	Sorting of the analyses to be used in the Feedforward Neural Network clinopyroxene-only geoT-P (https://lorenzochicchi-deeplearning-thermobarometry-main-b2fjar.streamlit.app/). Copy and paste values from data output, sort the data according to column CA (true/false), and then paste the index, sample, and cpx components in the empty form file of the app. Drag and drop the empty form in the Deeplearning app, run the app and then save and copy the results in columns F-I of the data sorting sheet")
+
+st.markdown('''
+<style>
+[data-testid="stMarkdownContainer"] ul{
+    padding-left:40px;
+}
+</style>
+''', unsafe_allow_html=True)
+
+st.write("The input dataset must be a .xlsx file with the following structure:")
+st.write("**Only clinopyroxene dataset:**")
+df = pd.read_excel('pages/Example_input.xlsx')
+st.table(df)
+
+df_empy = pd.read_excel('pages/Form_input.xlsx') 
+df_xlsx = to_excel(df_empy)
+st.download_button(label='Download an empty form here!',
+                                data=df_xlsx ,
+                                file_name= 'Empty_form.xlsx')
+
+
 st.header("References")
 col1, col2,col3, col4 = st.columns([1,1.45,1.5,1])
 
@@ -44,16 +80,6 @@ with col4:
     im = Image.open("imgs/lorenzo.jpeg")
     st.image(im,use_column_width=True, caption= 'Università degli Studi di Firenze, Dipartimento di Fisica e Astrofisica, INFN')
 
-st.header("Input structure")
-st.write("The input dataset must be a .xlsx file with the following structure:")
-st.write("**Only clinopyroxene dataset:**")
-df = pd.read_excel('pages/Example_input.xlsx')
-st.table(df)
 
-df_empy = pd.read_excel('pages/Form_input.xlsx') 
-df_xlsx = to_excel(df_empy)
-st.download_button(label='Download an empty form here!',
-                                data=df_xlsx ,
-                                file_name= 'Empty_form.xlsx')
 
 
