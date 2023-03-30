5fd6cb6788d2def6fd1857c46f14b2c764105475
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-03-30 16:19:53 +0200

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -37,11 +37,11 @@ st.download_button(label='Download the calculation file',
                                 file_name= 'Calculation.xlsx')
 
 st.write("To carry out the processing it is necessary to download the Calculation.xlsx file with the botton above and follow the subsequent steps, each relating to a sheet of the file.")
-st.markdown("- **data input**: Clinopyroxene analyses. Input the analyses (paste special values) as indicated in the example (blank cell if the oxide has not been analysed or is below detection limit)")
-st.markdown("- **calculation-1**:	Calculation of clinopyroxene formula based on 4 cations and Fe3+ on charge balance. Do use the fill down command from column A to column CR to perform the calculation on all the analyses")
-st.markdown("- **calculation-2**:	Calculation of clinopyroxene components. Do use the fill down command from column A to FF to perform the calculation on all the analyses")
-st.markdown("- **data output**: -	Do use the fill down command from column A to BV to perform the check on all the analyses")
-st.markdown("- **data sorting** -	Sorting of the analyses to be used in the Feedforward Neural Network clinopyroxene-only geoT-P. Copy and paste values from data output, sort the data according to column CA (true/false), and then paste the index, sample, and cpx components in the empty form file of the app (button below). Drag and drop the empty form in the Home page, run the app and then save and copy the results in columns F-I of the data sorting sheet")
+st.markdown("- **data input**: Clinopyroxene analyses. Input the analyses (paste special values) as indicated in the example (blank cell if the oxide has not been analysed or is below detection limit).")
+st.markdown("- **calculation-1**:	Calculation of clinopyroxene formula based on 4 cations and Fe3+ on charge balance. Do use the fill down command from column A to column CR to perform the calculation on all the analyses.")
+st.markdown("- **calculation-2**:	Calculation of clinopyroxene components. Do use the fill down command from column A to FF to perform the calculation on all the analyses.")
+st.markdown("- **data output**: -	Do use the fill down command from column A to BV to perform the check on all the analyses.")
+st.markdown("- **data sorting** -	Sorting of the analyses to be used in the Feedforward Neural Network clinopyroxene-only geoT-P. Copy and paste values from data output, sort the data according to column CA (true/false), and then paste the index, sample, and cpx components in the empty form file of the app (button below). Drag and drop the empty form in the Home page, run the app and then save and copy the results in columns F-I of the data sorting sheet.")
 
 st.markdown('''
 <style>
@@ -64,13 +64,12 @@ df = pd.read_excel('pages/Example_input.xlsx')
 st.table(df)
 
 
-
 st.header("References")
 col1, col2,col3, col4 = st.columns([1,1.45,1.5,1])
 
 with col1:
     im = Image.open("imgs/lorenzo.jpeg")
-    st.image(im,use_column_width=True, caption= 'Lorenzo Chicci, Università degli Studi di Firenze, Dipartimento di Fisica e Astrofisica, INFN')
+    st.image(im,use_column_width=True, caption='Lorenzo Chicchi, Università degli Studi di Firenze, Dipartimento di Fisica e Astrofisica, INFN')
 with col2:
     #st.write("#")
     im = Image.open("imgs/luca.png")
