2e7ae5ab97cf6f037cd1ec12d8c42b69bc87f9a8
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-04-03 14:04:16 +0200

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -13,15 +13,15 @@ def to_excel(df):
     df.to_excel(writer, index=False, sheet_name='Sheet1')
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
+    #format1 = workbook.add_format({'num_format': '0.00'}) 
+    #worksheet.set_column('A:A', None, format1)  
     writer.save()
     processed_data = output.getvalue()
     return processed_data
 
 im = Image.open("logo_noBG.png")
 st.set_page_config(
-    page_title="Deep Thermobarometer",
+    page_title="Gaia - Info",
     page_icon=im,
     layout="wide"
 )
