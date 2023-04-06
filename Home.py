5188c280b5cd66891b6bc52cc0cf728f8f6381e8
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-04-06 15:52:54 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -143,8 +143,10 @@ input_example =  pd.read_excel('files/input_example.xlsx')
 st.dataframe(input_example)
 
 st.markdown("The columns ***Index***, ***sample***, ***notes*** and ***notes*** can be used to identify the samples.")
-st.markdown("The columns, ***SiO2***, ***TiO2***, ***Al2O3***, ***Cr2O3***,***FeO tot***,***MnO***,***NiO***, ***MgO***, ***CaO***, ***Na2O*** and ***K2O*** \
-             must be filled with the analyses.")
+st.markdown("The columns, ***SiO2***, ***TiO2***, ***Al2O3***, ***Cr2O3***,***FeO tot***,***MnO***,***NiO***, ***MgO***, ***CaO***, ***Na2O***, ***K2O*** and ***tot** \
+             must be filled with the oxides analyses. If the oxide has not been analysed or is below detection limit the cell can be leave blank or set to zero. \
+             The same can be done if the total has not been calculated.  
+             An empty file with the rigth structure can be downloaded by using the botton below.")
             
             
 df_input_sheet = pd.read_excel('files/input_sheet.xlsx')
