c0b2ef6bc7856845063965e190fb1c05090a0e49
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-04-06 17:28:27 +0200

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -44,42 +44,11 @@ st.set_page_config(
 
 st.title("Info")
 
-st.header("Instructions")
+st.header("How GAIA works")
 
 
     
-st.write("Before using the deep learning model is necessary to make a calculation of clinopyroxene components and check on analysis quality as described in [].")
-    
-dic_cal = pd.read_excel('pages/Calculation.xlsx', sheet_name=None)
-df_calc_xlsx = to_excel_multi_sheet(dic_cal)
-st.download_button(label='Download the calculation file', data=df_calc_xlsx , file_name= 'Calculation.xlsx')
-
-st.write("To carry out the processing it is necessary to download the Calculation.xlsx file with the botton above and follow the subsequent steps, each relating to a sheet of the file.")
-st.markdown("- **data input**: Clinopyroxene analyses. Input the analyses (paste special values) as indicated in the example (blank cell if the oxide has not been analysed or is below detection limit).")
-st.markdown("- **calculation-1**:	Calculation of clinopyroxene formula based on 4 cations and Fe3+ on charge balance. Do use the fill down command from column A to column CR to perform the calculation on all the analyses.")
-st.markdown("- **calculation-2**:	Calculation of clinopyroxene components. Do use the fill down command from column A to FF to perform the calculation on all the analyses.")
-st.markdown("- **data output**: -	Do use the fill down command from column A to BV to perform the check on all the analyses.")
-st.markdown("- **data sorting** -	Sorting of the analyses to be used in the Feedforward Neural Network clinopyroxene-only geoT-P. Copy and paste values from data output, sort the data according to column CA (true/false), and then paste the index, sample, and cpx components in the empty form file of the app (button below). Drag and drop the empty form in the Home page, run the app and then save and copy the results in columns F-I of the data sorting sheet.")
-
-st.markdown('''
-<style>
-[data-testid="stMarkdownContainer"] ul{
-    padding-left:40px;
-}
-</style>
-''', unsafe_allow_html=True)
-
-df_empy = pd.read_excel('pages/Form_input.xlsx') 
-df_xlsx = to_excel(df_empy)
-st.download_button(label='Download an empty form here!',
-                                data= df_xlsx ,
-                                file_name= 'Empty_form.xlsx')
-
-
-st.write("A the end of the elaboration the input dataset must be a .xlsx file with the following structure:")
-st.write("**Only clinopyroxene dataset:**")
-df = pd.read_excel('pages/Example_input.xlsx')
-st.table(df)
+st.write('Working in progress..')
 
 
 st.header("References")
