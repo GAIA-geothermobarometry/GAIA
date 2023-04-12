2da66791d03c5402c8089a6c32b14c79fa0113ba
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-04-12 10:13:25 +0200

    Update Info.py

--- pages/Info.py
+++ pages/Info.py
@@ -48,39 +48,15 @@ st.title("Info")
 
 
     
-st.write("The anatomy of the plumbing system of active volcanoes is fundamental to understand\
-how magma is stored and channeled to the surface. Reliable geothermobarometric\
-estimates are, therefore, critical to assess the depths and temperatures of the complex\
-system of magmatic reservoirs that form a volcano apparatus. Here, we developed a\
-novel Machine Learning approach based upon Feedforward Neural Networks to\
-estimate P-T conditions of magma (clinopyroxene) storage and migration within the\
-crust. Our Feedforward Neural Network method applied to clinopyroxene compositions\
-yields better uncertainties (Root-Mean-Square Error and R2 score) than previous\
-Machine Learning methods and set the basis for a novel generation of reliable\
-geothermobarometers, which extends beyond the paradigm associated to crystal-liquid\
-equilibrium. Also, the bootstrap procedure, inherent to the Feedforward Neural Network\
-architecture, permits to perform a rigorous assessment of the P-T uncertainty\
-associated to each clinopyroxene composition, as opposed to the Root-Mean-Square\
-Error representing the P-T uncertainty of whole set of clinopyroxene compositions.\
-As a test, we applied our clinopyroxene-only Feedforward Neural Network\
-geothermobarometer to assess P-T conditions of five Italian volcanoes (Somma-\
-Vesuvio, Campi Flegrei, Etna, Stromboli, Volcano), which are among the most\
-dangerous volcanic centres in Europe. The results on the depths of the plumbing\
-systems are in excellent agreement with those obtained with independent geophysical\
-and geodetic surveys, and provide further evidence to current models of volcano\
-plumbing systems consisting of physically-separated reservoirs interconnected by a\
-network of conduits channelling magma en route to the surface. The results on the\
-magma (clinopyroxene crystallization) temperatures are also in agreement with other\
-estimates, albeit obtained considering - mainly but not only - thermodynamically-based\
-clinopyroxene-liquid geothermometers.\
-The clinopyroxene-only Feedforward Neural Network geothermobarometer presented\
-in this study can set robust estimates of magma storage, segregation, and ascent\
-conditions within the plumbing system of active volcanoes, helping to unravel P-T\
-variations, if any, during their eruptive history and providing robust clues to volcanic\
-hazard assessment.")
+st.write("The anatomy of the plumbing system of active volcanoes is fundamental to understanding how magma is stored and channeled to the surface. \
+Reliable geothermobarometric estimates are, therefore, critical to assess the depths and temperatures of the complex system of magmatic reservoirs \ 
+that form a volcano apparatus. Here, we developed a novel Machine Learning approach based upon Feedforward Neural Networks (GAIA) to estimate P-T \
+conditions of magma storage and migration within the crust. Our Feedforward Neural Network method applied to clinopyroxene compositions yields better \
+uncertainties (Root-Mean-Square Error and R2 score) than previous Machine Learning methods and set the basis for a novel generation of reliable geothermobarometers, which extends beyond the paradigm associated to crystal-liquid equilibrium. Also, the bootstrap procedure, inherent to the Feedforward Neural Network architecture, permits to perform a rigorous assessment of the P-T uncertainty associated to each clinopyroxene composition, as opposed to the Root-Mean-Square Error representing the P-T uncertainty of whole set of clinopyroxene compositions.")
 
+st.write("GAIA can set robust estimates of magma storage, segregation, and ascent conditions within the plumbing system of active volcanoes, helping to unravel P-T variations, if any, during their eruptive history and providing robust clues to volcanic hazard assessment.")
 
-st.header("References")
+st.header("The people")
 col1, col2,col3, col4 = st.columns([1,1.45,1.5,1])
 
 with col1:
