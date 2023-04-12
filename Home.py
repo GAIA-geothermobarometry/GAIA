93394c63907914d7310a27d2467e0908cd2f12b0
GAIA-geothermobarometry <129048064+GAIA-geothermobarometry@users.noreply.github.com>
2023-04-12 11:25:44 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -132,7 +132,7 @@ if st.button('Preprocess data and make predictions'):
     for i  in range(len(dfs)):
       col_tuple = [(global_labels[i], c) for c in dfs[i]]
       cols = pd.MultiIndex.from_tuples(col_tuple)
-      new_dfs.append(pd.DataFrame(dfs[i].values, columns= pd.MultiIndex.from_tuples(col_tuple), index = dfs[i].index)
+      new_dfs.append(pd.DataFrame(dfs[i].values, columns= pd.MultiIndex.from_tuples(col_tuple), index = dfs[i].index))
     
     empty_col = pd.DataFrame(columns=pd.MultiIndex.from_tuples([('-','-')]))
     
